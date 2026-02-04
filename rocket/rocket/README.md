368e69ae93d30d0d97f50d60452bb700bdd6afdc
Antoine Boubée <antoine.boubee@student-cs.fr>
2026-02-04 17:35:42 +0100

    updt

---
+++ rocket/rocket/README.md
@@ -0,0 +1,24 @@
+Implementation d'une organisation de cartes auto-organisatrices pour prédire la poussée à donner à la fusée en fonction de la vitesse et de l'erreur de position.
+
+J'essaie actuellement de comprendre comment les exemples cxsom fonctionnent pour adapter ma propre implémentation.
+
+Le code présent permet de definir l'architecture des cartes et les relations entre elles. 
+
+## Suivre la procédure suivante pour tester
+
+### Permet de creer un root-dir, et de configurer les variables pour le processeur.
+```
+mkdir root-dir
+make cxsom-set-config ROOT_DIR=./root-dir VENV=../cxsom-venv HOSTNAME=localhost PORT=10000 SKEDNET_PORT=20000 NB_THREADS=4
+```
+
+### Permet de lancer le processeur et scanner le root-dir (pour le moment vide)
+```
+make cxsom-launch-processor
+make cxsom-scan-vars
+```
+
+### Envoyer les regles au processeur (le root-dir se remplit)
+```
+make send-rules
+```
--- rocket/rocket/xsom.cpp
+++ rocket/rocket/xsom.cpp
@@ -7,6 +7,7 @@
 #define CACHE              2
 
 #define TRACE          10000
+#define SAVE_TRACE      1000
 #define OPENED          true
 #define OPEN_AS_NEEDED false
 #define FORGET             0
@@ -15,20 +16,19 @@
 
 #define MAP_SIZE        1500
 
+// cxsom declarations
 using namespace cxsom::rules;
 context* cxsom::rules::ctx = nullptr;
 
 // ####################
 // #                  #
-// #  Building arch   #
+// #  Building archi  #
 // #                  #
 // ####################
 
 int main(int argc, char* argv[]) {
   context c(argc, argv);
 
-  auto archi = cxsom::builder::architecture();
-  
   kwd::parameters p_main, p_match, p_learn, p_learn_e, p_learn_c, p_external, p_contextual, p_global;
   p_main       | kwd::use("walltime", WALLTIME), kwd::use("epsilon", 0);
   p_match      | p_main, kwd::use("sigma", .2);
@@ -38,17 +38,14 @@ int main(int argc, char* argv[]) {
   p_external   | p_main;
   p_contextual | p_main;
   p_global     | p_main, kwd::use("random-bmu", 1), kwd::use("sigma", .01), kwd::use("beta", .5), kwd::use("delta", .01), kwd::use("deadline", 100);
-  
-  auto map_settings = cxsom::builder::map::make_settings();
-  map_settings.map_size          = MAP_SIZE;
-  map_settings.cache_size        = CACHE;
-  map_settings.weights_file_size = TRACE;
-  map_settings.kept_opened       = OPENED;
-  map_settings                   = {p_external, p_contextual, p_global};
 
-  auto error  = cxsom::builder::variable("in", cxsom::builder::name("error"),  "Scalar", CACHE, TRACE, OPENED);
-  auto speed  = cxsom::builder::variable("in", cxsom::builder::name("speed"),  "Scalar", CACHE, TRACE, OPENED);
-  auto thrust = cxsom::builder::variable("in", cxsom::builder::name("thrust"), "Scalar", CACHE, TRACE, OPENED);
+  auto archi = cxsom::builder::architecture();
+
+
+
+  auto error  = cxsom::builder::variable("in", cxsom::builder::name("xi_error"),  "Scalar", CACHE, TRACE, OPENED);
+  auto speed  = cxsom::builder::variable("in", cxsom::builder::name("xi_speed"),  "Scalar", CACHE, TRACE, OPENED);
+  auto thrust = cxsom::builder::variable("in", cxsom::builder::name("xi_thrust"), "Scalar", CACHE, TRACE, OPENED);
 
   error->definition();
   speed->definition();
@@ -57,28 +54,49 @@ int main(int argc, char* argv[]) {
   auto errorMap  = cxsom::builder::map::make_1D("error");
   auto speedMap  = cxsom::builder::map::make_1D("speed");
   auto thrustMap = cxsom::builder::map::make_1D("thrust");
-  
+
+  auto map_settings = cxsom::builder::map::make_settings();
+  map_settings.map_size          = MAP_SIZE;
+  map_settings.cache_size        = CACHE;
+  map_settings.weights_file_size = TRACE;
+  map_settings.kept_opened       = OPENED;
+  map_settings                   = {p_external, p_contextual, p_global};
+
+  std::vector<cxsom::builder::Map::Layer*> layers; // This will store all layers 
+  auto out_layer = std::back_inserter(layers); 
+
   // Liens entre les inputs et les cartes
-  errorMap->external   (error,    fx::match_gaussian, p_match, fx::learn_triangle, p_learn_e);
-  speedMap->external   (speed,    fx::match_gaussian, p_match, fx::learn_triangle, p_learn_e);
-  thrustMap->external  (thrust,   fx::match_gaussian, p_match, fx::learn_triangle, p_learn_e);
+  *(out_layer++) = errorMap->external   (error,    fx::match_gaussian, p_match, fx::learn_triangle, p_learn_e);
+  *(out_layer++) = speedMap->external   (speed,    fx::match_gaussian, p_match, fx::learn_triangle, p_learn_e);
+  *(out_layer++) = thrustMap->external  (thrust,   fx::match_gaussian, p_match, fx::learn_triangle, p_learn_e);
 
   // Liens entre les cartes
-  errorMap->contextual (speedMap, fx::match_gaussian, p_match, fx::learn_triangle, p_learn_c);
-  speedMap->contextual (thrustMap, fx::match_gaussian, p_match, fx::learn_triangle, p_learn_c);
-  thrustMap->contextual(errorMap, fx::match_gaussian, p_match, fx::learn_triangle, p_learn_c);
-  errorMap->contextual (thrustMap, fx::match_gaussian, p_match, fx::learn_triangle, p_learn_c);
-  speedMap->contextual (errorMap, fx::match_gaussian, p_match, fx::learn_triangle, p_learn_c);
-  thrustMap->contextual(speedMap, fx::match_gaussian, p_match, fx::learn_triangle, p_learn_c);
+  *(out_layer++) = errorMap ->contextual (speedMap, fx::match_gaussian, p_match, fx::learn_triangle, p_learn_c);
+  *(out_layer++) = speedMap ->contextual (thrustMap,fx::match_gaussian, p_match,fx::learn_triangle,  p_learn_c);
+  *(out_layer++) = thrustMap->contextual (errorMap, fx::match_gaussian, p_match, fx::learn_triangle, p_learn_c);
+  *(out_layer++) = errorMap ->contextual (thrustMap,fx::match_gaussian, p_match,fx::learn_triangle,  p_learn_c);
+  *(out_layer++) = speedMap ->contextual (errorMap, fx::match_gaussian, p_match, fx::learn_triangle, p_learn_c);
+  *(out_layer++) = thrustMap->contextual (speedMap, fx::match_gaussian, p_match, fx::learn_triangle, p_learn_c);
+
 
   archi << errorMap << speedMap << thrustMap;
   *archi = map_settings;
-  archi->realize();
-  errorMap->internals_random_at(0);
-  speedMap->internals_random_at(0);
-  thrustMap->internals_random_at(0);
 
-  std::ofstream dot_file("3soms-archi.dot");
+  archi->realize();
+  
+  for(auto map : archi->maps) map->internals_random_at(0);
+
+  int save_period = TRACE / SAVE_TRACE;
+
+  // Saving weights
+  for(auto layer_ptr : layers) {
+    auto W = layer_ptr->_W();
+    auto Wsaved = cxsom::builder::variable("save", W->varname, W->type, CACHE, SAVE_TRACE, OPEN_AS_NEEDED);
+    Wsaved->definition();
+    Wsaved->var() << fx::copy(kwd::times(W->var(), save_period)) | kwd::use("walltime", WALLTIME);
+  }
+
+  std::ofstream dot_file("architecture/3soms-archi.dot");
   dot_file << archi->write_dot;
 
   return 0;
