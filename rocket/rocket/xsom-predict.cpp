ae527367eb50bd609f3e7e6677c21c83a001ff97
Antoine Boubée <antoine.boubee@student-cs.fr>
2026-02-10 18:55:25 +0100

    update

---
+++ rocket/rocket/xsom-predict.cpp
@@ -0,0 +1,113 @@
+#include "xsom.hpp"
+
+// // ############################
+// // #                          #
+// // #  Building archi predict  #
+// // #                          #
+// // ############################
+
+int main(int argc, char *argv[]) {
+  context c(argc, argv);
+
+  int saved_weight_at = 2600;
+  std::string wtype = "type";
+  Params params;
+
+  auto archi = cxsom::builder::architecture();
+
+  // definition des cartes
+  auto errorMap = cxsom::builder::map::make_1D("error");
+  auto speedMap = cxsom::builder::map::make_1D("speed");
+  auto thrustMap = cxsom::builder::map::make_1D("thrust");
+
+  auto map_settings = cxsom::builder::map::make_settings();
+  map_settings.map_size = MAP_SIZE;
+  map_settings.cache_size = CACHE;
+  map_settings.weights_file_size = TRACE;
+  map_settings.kept_opened = OPENED;
+  map_settings = {params.p_external, params.p_contextual, params.p_global};
+
+  auto errorWc0 = cxsom::builder::variable(
+      "save", cxsom::builder::name("error") / cxsom::builder::name("Wc-0"),
+      wtype, CACHE, TRACE, OPENED);
+  auto errorWc1 = cxsom::builder::variable(
+      "save", cxsom::builder::name("error") / cxsom::builder::name("Wc-1"),
+      wtype, CACHE, TRACE, OPENED);
+  auto speedWc0 = cxsom::builder::variable(
+      "save", cxsom::builder::name("speed") / cxsom::builder::name("Wc-0"),
+      wtype, CACHE, TRACE, OPENED);
+  auto speedWc1 = cxsom::builder::variable(
+      "save", cxsom::builder::name("speed") / cxsom::builder::name("Wc-1"),
+      wtype, CACHE, TRACE, OPENED);
+  auto thrustWc0 = cxsom::builder::variable(
+      "save", cxsom::builder::name("thrust") / cxsom::builder::name("Wc-0"),
+      wtype, CACHE, TRACE, OPENED);
+  auto thrustWc1 = cxsom::builder::variable(
+      "save", cxsom::builder::name("thrust") / cxsom::builder::name("Wc-1"),
+      wtype, CACHE, TRACE, OPENED);
+  errorMap->contextual(speedMap, fx::match_gaussian, params.p_match, errorWc0,
+                       saved_weight_at);
+  errorMap->contextual(thrustMap, fx::match_gaussian, params.p_match, errorWc1,
+                       saved_weight_at);
+  speedMap->contextual(errorMap, fx::match_gaussian, params.p_match, speedWc0,
+                       saved_weight_at);
+  speedMap->contextual(thrustMap, fx::match_gaussian, params.p_match, speedWc1,
+                       saved_weight_at);
+  thrustMap->contextual(errorMap, fx::match_gaussian, params.p_match, thrustWc0,
+                        saved_weight_at);
+  thrustMap->contextual(speedMap, fx::match_gaussian, params.p_match, thrustWc1,
+                        saved_weight_at);
+
+  // declaration des inputs et de la sortie
+  auto error =
+      cxsom::builder::variable("in", cxsom::builder::name("predict_error"),
+                               "Scalar", CACHE, TRACE, OPENED);
+  auto speed =
+      cxsom::builder::variable("in", cxsom::builder::name("predict_speed"),
+                               "Scalar", CACHE, TRACE, OPENED);
+  auto thrust = cxsom::builder::variable("predict-out",
+                                         cxsom::builder::name("predict_thrust"),
+                                         "Scalar", CACHE, TRACE, OPENED);
+
+  error->definition();
+  speed->definition();
+  thrust->definition();
+
+  auto errorWe0 = cxsom::builder::variable(
+      "save", cxsom::builder::name("error") / cxsom::builder::name("We0"),
+      wtype, CACHE, TRACE, OPENED);
+  auto speedWe0 = cxsom::builder::variable(
+      "save", cxsom::builder::name("speed") / cxsom::builder::name("We0"),
+      wtype, CACHE, TRACE, OPENED);
+  auto thrustWe0 = cxsom::builder::variable(
+      "save", cxsom::builder::name("thrust") / cxsom::builder::name("We0"),
+      wtype, CACHE, TRACE, OPENED);
+
+  errorMap->external(error, fx::match_gaussian, params.p_match, errorWe0,
+                     saved_weight_at);
+  speedMap->external(speed, fx::match_gaussian, params.p_match, speedWe0,
+                     saved_weight_at);
+
+  errorWe0->definition();
+  errorWc0->definition();
+  errorWc1->definition();
+  speedWe0->definition();
+  speedWc0->definition();
+  speedWc1->definition();
+  thrustWe0->definition();
+  thrustWc0->definition();
+  thrustWc1->definition();
+
+  archi << errorMap << speedMap << thrustMap;
+  *archi = map_settings;
+
+  archi->realize();
+  {
+    std::ofstream dot_file("predict.dot");
+    dot_file << archi->write_dot;
+  }
+
+  thrust->var() << fx::value_at(kwd::at(thrustWe0->var(), saved_weight_at),
+                                thrustMap->output_BMU()->var()) |
+      kwd::use("walltime", WALLTIME);
+}
---
+++ rocket/rocket/xsom-train.cpp
@@ -0,0 +1,95 @@
+#include "xsom.hpp"
+
+// ####################
+// #                  #
+// #  Building archi  #
+// #                  #
+// ####################
+
+int main(int argc, char *argv[]) {
+  context c(argc, argv);
+
+  auto archi = cxsom::builder::architecture();
+  Params params;
+
+  auto error_input = cxsom::builder::variable(
+      "in", cxsom::builder::name("xi_error"), "Scalar", CACHE, TRACE, OPENED);
+  auto speed_input = cxsom::builder::variable(
+      "in", cxsom::builder::name("xi_speed"), "Scalar", CACHE, TRACE, OPENED);
+  auto thrust_input = cxsom::builder::variable(
+      "in", cxsom::builder::name("xi_thrust"), "Scalar", CACHE, TRACE, OPENED);
+
+  error_input->definition();
+  speed_input->definition();
+  thrust_input->definition();
+
+  auto errorMap = cxsom::builder::map::make_1D("error");
+  auto speedMap = cxsom::builder::map::make_1D("speed");
+  auto thrustMap = cxsom::builder::map::make_1D("thrust");
+
+  auto map_settings = cxsom::builder::map::make_settings();
+  map_settings.map_size = MAP_SIZE;
+  map_settings.cache_size = CACHE;
+  map_settings.weights_file_size = TRACE;
+  map_settings.kept_opened = OPENED;
+  map_settings = {params.p_external, params.p_contextual, params.p_global};
+
+  std::vector<cxsom::builder::Map::Layer *> layers;
+  auto out_layer = std::back_inserter(layers);
+
+  // Liens entre les inputs et les cartes
+  *(out_layer++) =
+      errorMap->external(error_input, fx::match_gaussian, params.p_match,
+                         fx::learn_triangle, params.p_learn_e);
+  *(out_layer++) =
+      speedMap->external(speed_input, fx::match_gaussian, params.p_match,
+                         fx::learn_triangle, params.p_learn_e);
+  *(out_layer++) =
+      thrustMap->external(thrust_input, fx::match_gaussian, params.p_match,
+                          fx::learn_triangle, params.p_learn_e);
+
+  // Liens entre les cartes
+  *(out_layer++) =
+      errorMap->contextual(speedMap, fx::match_gaussian, params.p_match,
+                           fx::learn_triangle, params.p_learn_c);
+  *(out_layer++) =
+      speedMap->contextual(thrustMap, fx::match_gaussian, params.p_match,
+                           fx::learn_triangle, params.p_learn_c);
+  *(out_layer++) =
+      thrustMap->contextual(errorMap, fx::match_gaussian, params.p_match,
+                            fx::learn_triangle, params.p_learn_c);
+  *(out_layer++) =
+      errorMap->contextual(thrustMap, fx::match_gaussian, params.p_match,
+                           fx::learn_triangle, params.p_learn_c);
+  *(out_layer++) =
+      speedMap->contextual(errorMap, fx::match_gaussian, params.p_match,
+                           fx::learn_triangle, params.p_learn_c);
+  *(out_layer++) =
+      thrustMap->contextual(speedMap, fx::match_gaussian, params.p_match,
+                            fx::learn_triangle, params.p_learn_c);
+
+  archi << errorMap << speedMap << thrustMap;
+  *archi = map_settings;
+
+  archi->realize();
+
+  for (auto map : archi->maps)
+    map->internals_random_at(0);
+
+  int save_period = TRACE / SAVE_TRACE;
+
+  // Saving weights
+  for (auto layer_ptr : layers) {
+    auto W = layer_ptr->_W();
+    auto Wsaved = cxsom::builder::variable("save", W->varname, W->type, CACHE,
+                                           SAVE_TRACE, OPEN_AS_NEEDED);
+    Wsaved->definition();
+    Wsaved->var() << fx::copy(kwd::times(W->var(), save_period)) |
+        kwd::use("walltime", WALLTIME);
+  }
+
+  std::ofstream dot_file("architecture/3soms-archi.dot");
+  dot_file << archi->write_dot;
+
+  return 0;
+}
